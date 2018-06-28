7a1de32f480b79a653e82404aefbc74151a448b3
suibian <1062415295@qq.com>
2018-06-28 22:35:23 +0800

    添加TestService文件

---
+++ src/com/shsxt/service/TestService.java
@@ -0,0 +1,10 @@
+package com.shsxt.service;
+
+/**
+ * Created by Administrator on 2018/6/28.
+ */
+public class TestService {
+    public static void main(String[] args) {
+        System.out.println("TestService...");
+    }
+}
