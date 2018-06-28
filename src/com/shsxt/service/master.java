019b5b233803ec480b79bb9453744afa8f8fb4d4
suibian <1062415295@qq.com>
2018-06-29 01:38:24 +0800

    添加master文件

---
+++ src/com/shsxt/service/master.java
@@ -0,0 +1,9 @@
+package com.shsxt.service;
+
+/**
+ * Created by Administrator on 2018/6/29.
+ */
+public class master {public static void main(String[] args) {
+    System.out.println("AccountServicec....");
+}
+}
